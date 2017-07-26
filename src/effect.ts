b9f60fcde8ccc3a6c81450de1640fcf6a309a55e
Simon Højberg <r.hackr@gmail.com>
2017-07-25 23:34:52 -0400

    Make Effect a functor

--- src/effect.ts
+++ src/effect.ts
@@ -37,6 +37,14 @@ class _Effect {
     });
   }
 
+  map(f): Effect {
+    return Effect(done => {
+      this.run(msg => {
+        done(f(msg));
+      });
+    });
+  }
+
   run(updater: Updater) {
     this.runner.ap(Just(updater));
   }
