dbb890883fba936998bb3080620b514d708f24e8
Andrew Goodale <andrewgoodale@upserve.com>
2017-09-19 11:14:24 -0400

    Add type declarations

---
+++ index.d.ts
@@ -0,0 +1,61 @@
+// Type definitions for gongfu
+// Project: https://github.com/hojberg/gongfu
+// Definitions by: Andrew Goodale <https://github.com/newyankeecodeshop>
+// TypeScript Version: 2.3
+
+import { Component } from "react";
+
+export interface Msg {
+  tag: string;
+}
+
+export type MsgConstructor = (a?: any) => Msg;
+export type Updater = (msg: Msg) => void;
+export type Runner = (updater: Updater) => void;
+export type Setup = (onChange: Updater) => void;
+
+export interface Effect {
+  concat(that: Effect): Effect;
+
+  map(f: (a: any) => any): Effect;
+
+  run(updater: Updater): void;
+}
+
+export function Effect(runner?: Runner): Effect;
+
+export namespace Effect {
+  function of(runner?: Runner): Effect;
+  function empty(): Effect;
+}
+
+export interface ModelWithEffect<M> {
+  model: M;
+  effect: Effect;
+}
+
+export interface Sub {
+  run(updater: Updater): void;
+}
+
+export function Sub(Msg?: MsgConstructor, setup?: Setup): Sub;
+
+export namespace Sub {
+  function of(Msg?: MsgConstructor, setup?: Setup): Sub;
+  function none(): Sub;
+}
+
+export function updaterFor(updater: Updater, Msg: MsgConstructor): Updater;
+
+export interface ProgramProps {
+  Component: any;
+  update: (msg: Msg, model: any) => ModelWithEffect<any>;
+  init: () => ModelWithEffect<any>;
+  subscriptions?: (model: any) => Sub;
+}
+
+export interface ProgramState {
+  model: any;
+}
+
+export class Program extends Component<ProgramProps, ProgramState> {}
