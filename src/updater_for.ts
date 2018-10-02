f7617ff9ecd53dc5acade4ee0e34242244f32d40
Andrew Goodale <andrewgoodale@upserve.com>
2018-10-02 11:56:09 -0400

    Cache updaters to reduce GC from rendering

--- src/updater_for.ts
+++ src/updater_for.ts
@@ -1,9 +1,28 @@
 import { Msg, MsgConstructor } from "./msg";
 
 type Updater = (msg: Msg) => void;
+type UpdaterCache = Map<MsgConstructor, Map<Updater, Updater>>;
+
+// Not using WeakMap because updaters and MsgConstructors are const globals
+const updaterCache: UpdaterCache = new Map();
+
+function getUpdaterMap(msg: MsgConstructor): Map<Updater, Updater> {
+  let updaterMap = updaterCache.get(msg);
+
+  if (updaterMap == null) {
+    updaterCache.set(msg, updaterMap = new Map());
+  }
+  return updaterMap;
+}
 
 function updaterFor(updater: Updater, Msg: MsgConstructor): Updater {
-  return (msgData: any) => updater(Msg(msgData));
+  const updaterMap = getUpdaterMap(Msg);
+  let cachedUpdater = updaterMap.get(updater);
+
+  if (cachedUpdater == null) {
+    updaterMap.set(updater, cachedUpdater = (childMsg) => updater(Msg(childMsg)));
+  }
+  return cachedUpdater;
 }
 
 export default updaterFor;
